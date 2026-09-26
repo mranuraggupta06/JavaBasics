0c6a619e18f27352e2c6917213e378152423e73c
Anurag Gupta <mranuraggpta06@gmail.com>
2026-09-26 20:39:06 +0530

    "learn java fundamentals "

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,10 @@
 public class Main{
-
+    public static void main(String[] args){
+        int x, y, z;
+        x = y = z = 2;
+        x += y;
+        y -= z;
+        z /= (x + y);
+        System.out.println(x + " " + y + " " + z);
+    }
 }
